283c6ef1a48d40a611adc6f518c8c42737078f17
StonyTark1117 <braydonsanders2001@gmail.com>
2025-10-13 22:59:15 -0700

    Initial worldgen framework completed.

--- src/main/java/com/stonytark/usefultoolsmod/datagen/DataGenerators.java
+++ src/main/java/com/stonytark/usefultoolsmod/datagen/DataGenerators.java
@@ -32,8 +32,8 @@ public class DataGenerators {
         BlockTagsProvider blockTagsProvider = new ModBlockTagProvider(packOutput, lookupProvider, existingFileHelper);
         generator.addProvider(event.includeServer(), blockTagsProvider);
         generator.addProvider(event.includeServer(), new ModItemTagProvider(packOutput, lookupProvider, blockTagsProvider.contentsGetter(), existingFileHelper));
-
         generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
+        generator.addProvider(event.includeServer(), new ModDatapackEntries(packOutput, lookupProvider));
     }
 }
